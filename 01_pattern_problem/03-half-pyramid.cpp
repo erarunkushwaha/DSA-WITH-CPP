cef74e243d801a2759df05af3a4d3571695da1e7
Arun Kushwaha <er.arunkushwaha@gmail.com>
2023-02-06 22:30:04 +0545

    half prymid introduced

---
+++ 01_pattern_problem/03-half-pyramid.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+/***
+ * print this
+ *              *
+ *              *   *
+ *              *   *   *
+ *              *   *   *   *
+ *              *   *   *   *   *
+ *              *   *   *   *   *   *
+ *              *   *   *   *   *   *   *
+ *              *   *   *   *   *   *   *   *
+ *              *   *   *   *   *   *   *   *   *
+ *              *   *   *   *   *   *   *   *   *   *
+ * 
+*/
+
+int main()
+{
+   int rowCount, colCount;
+   cout<<"Enter Number Of Row: ";
+   cin>>rowCount;
+   cout<<"Enter Number Of column: ";
+   cin>>colCount;
+
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int col = 0; col < row + 1; col++)
+        {
+            cout << "* ";
+        }
+        cout<<endl;
+    }
+}
