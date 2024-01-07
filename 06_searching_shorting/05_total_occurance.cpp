76d92da272bd96115c3b2d2264d1b1bdb039fcb3
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-01-07 21:00:53 +0545

    total coocurance added

---
+++ 06_searching_shorting/05_total_occurance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>  // std::cout
+#include <algorithm> // std::binary_search, std::sort
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7};
+    vector<int>::iterator low, up;
+    int target = 4;
+
+    low = lower_bound(v.begin(), v.end(), target);
+    up = upper_bound(v.begin(), v.end(), target);
+
+    cout
+        << "lower bound is :: " << (low - v.begin()) << endl;
+
+    cout << "upper bound is :: " << (up - v.begin()) << endl;
+
+    cout << "total occurancr is ::" << (up - v.begin()) -  (low - v.begin())  <<endl;
+        return 0;
+}
