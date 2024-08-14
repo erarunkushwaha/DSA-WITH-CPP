588aa6d640df68e11082212388a2da08be6f614f
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-08-14 13:08:44 +0545

    solve backspace sting by brutefore method

---
+++ JS/1 Array/2 STRING/1.backspace-string-compare.js
@@ -0,0 +1,38 @@
+/**
+ * 
+
+ Backspace String Compare
+
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty
+ */
+
+//  brute force method
+const removeHash = (string) => {
+  const array = string.split("");
+  let filteredArray = [];
+
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] !== "#") {
+      filteredArray.push(array[index]);
+    } else {
+      filteredArray.pop();
+    }
+  }
+  return filteredArray.join("");
+};
+
+let backspaceCompare = function (s, t) {
+  const string1 = removeHash(s);
+  const string2 = removeHash(t);
+  if (string1 === string2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const res = backspaceCompare("ab#c", "ad#c");
+
+console.log("res::", res);
