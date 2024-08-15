12e13cee7789b580332d51328d0b97b4e889ede2
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-08-15 13:37:57 +0545

    backspace string

--- JS/1 Array/2 STRING/1.backspace-string-compare.js
+++ JS/1 Array/2 STRING/1.backspace-string-compare.js
@@ -9,37 +9,30 @@ Note that after backspacing an empty text, the text will continue empty
  */
 
 //  brute force method
-// const removeHash = (string) => {
-//   const array = string.split("");
-//   let filteredArray = [];
+const removeHash = (string) => {
+  const array = string.split("");
+  let filteredArray = [];
 
-//   for (let index = 0; index < array.length; index++) {
-//     if (array[index] !== "#") {
-//       filteredArray.push(array[index]);
-//     } else {
-//       filteredArray.pop();
-//     }
-//   }
-//   return filteredArray.join("");
-// };
-
-// let backspaceCompare = function (s, t) {
-//   const string1 = removeHash(s);
-//   const string2 = removeHash(t);
-//   if (string1 === string2) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
-// const res = backspaceCompare("ab#c", "ad#c");
-
-// console.log("res::", res);
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] !== "#") {
+      filteredArray.push(array[index]);
+    } else {
+      filteredArray.pop();
+    }
+  }
+  return filteredArray.join("");
+};
 
 let backspaceCompare = function (s, t) {
-  let pointerS = s.length - 1;
-  let pointerT = t.length - 1;
+  const string1 = removeHash(s);
+  const string2 = removeHash(t);
+  if (string1 === string2) {
+    return true;
+  } else {
+    return false;
+  }
 };
-const res = backspaceCompare("ab#cd##", "ad#c");
+
+const res = backspaceCompare("ab#c", "ad#c");
+
 console.log("res::", res);
