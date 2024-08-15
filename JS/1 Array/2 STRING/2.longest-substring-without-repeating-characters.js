9b74ddfca997663500c64d54afbdae5ec67f9a85
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-08-15 14:17:57 +0545

    longest substring

---
+++ JS/1 Array/2 STRING/2.longest-substring-without-repeating-characters.js
@@ -0,0 +1,37 @@
+/**
+ * Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+ */
+const input = "pwwkew";
+
+var lengthOfLongestSubstring = function (s) {
+  if (s.length <= 1) return s.length;
+
+  let longest = 0;
+
+  for (let left = 0; left < s.length; left++) {
+    let seenChars = {},
+      currentLength = 0;
+
+    for (let right = left; right < s.length; right++) {
+      const currentChar = s[right];
+
+      if (!seenChars[currentChar]) {
+        currentLength++;
+        seenChars[currentChar] = true;
+        longest = Math.max(longest, currentLength);
+      } else {
+        break;
+      }
+    }
+  }
+
+  return longest;
+};
+
+const res = lengthOfLongestSubstring(input);
+
+console.log("res::", res);
