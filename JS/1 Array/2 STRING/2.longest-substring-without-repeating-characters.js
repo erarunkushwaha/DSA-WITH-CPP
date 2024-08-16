2e02a569e6688ce8c5df106149a420938ca5c8e8
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-08-16 12:51:03 +0545

    sliding window

--- JS/1 Array/2 STRING/2.longest-substring-without-repeating-characters.js
+++ JS/1 Array/2 STRING/2.longest-substring-without-repeating-characters.js
@@ -5,33 +5,58 @@ Given a string s, find the length of the longest
 substring
  without repeating characters.
  */
-const input = "pwwkew";
+// brute force
+// const input = "pwwkew";
 
-var lengthOfLongestSubstring = function (s) {
-  if (s.length <= 1) return s.length;
+// var lengthOfLongestSubstring = function (s) {
+//   if (s.length <= 1) return s.length;
 
-  let longest = 0;
+//   let longest = 0;
 
-  for (let left = 0; left < s.length; left++) {
-    let seenChars = {},
-      currentLength = 0;
+//   for (let left = 0; left < s.length; left++) {
+//     let seenChars = {},
+//       currentLength = 0;
 
-    for (let right = left; right < s.length; right++) {
-      const currentChar = s[right];
+//     for (let right = left; right < s.length; right++) {
+//       const currentChar = s[right];
 
-      if (!seenChars[currentChar]) {
-        currentLength++;
-        seenChars[currentChar] = true;
-        longest = Math.max(longest, currentLength);
-      } else {
-        break;
-      }
-    }
+//       if (!seenChars[currentChar]) {
+//         currentLength++;
+//         seenChars[currentChar] = true;
+//         longest = Math.max(longest, currentLength);
+//       } else {
+//         break;
+//       }
+//     }
+//   }
+
+//   return longest;
+// };
+
+// const res = lengthOfLongestSubstring(input);
+
+// console.log("res::", res);
+
+// sliding windiw--
+
+const string = "abcbdaac";
+
+const lengthOfLongestSubstring = function (s) {
+  if (s.length <= 1) {
+    return s.length;
   }
+  let longest = 0;
+  let left = 0;
+  let hashMap = {};
 
+  for (let right = 0; right < s.length; right++) {
+    if (hashMap[s[right]] >= left) {
+      left = hashMap[s[right]] + 1;
+    }
+    hashMap[s[right]] = right;
+    longest = Math.max(longest, right - left + 1);
+  }
   return longest;
 };
 
-const res = lengthOfLongestSubstring(input);
-
-console.log("res::", res);
+console.log(lengthOfLongestSubstring(string));
