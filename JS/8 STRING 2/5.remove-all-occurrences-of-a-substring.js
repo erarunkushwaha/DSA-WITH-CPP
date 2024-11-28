69f260d424ce1fbb3a474a68805352a71b590d9e
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-11-28 21:53:57 +0545

    removeOccurrences solved

---
+++ JS/8 STRING 2/5.remove-all-occurrences-of-a-substring.js
@@ -0,0 +1,10 @@
+var removeOccurrences = function (s, part) {
+  while (s.length != 0 && s.includes(part)) {
+    s = s.replaceAll(part, "");
+  }
+
+  return s;
+};
+
+console.log("test1::", removeOccurrences("daabcbaabcbc", "abc")); // dab
+console.log("test2::", removeOccurrences("axxxxyyyyb", "xy")); // ab
