3cbaa1e0bb5eedc1a9148d2d540795551ebcdc72
erarunkushwaha <er.arunkushwaha@gmail.com>
2025-07-30 08:43:41 +0530

    formated

--- JS/2_STRING/4.string-compression.ts
+++ JS/2_STRING/4.string-compression.ts
@@ -1,24 +1,18 @@
-
 // brute force method
 const compressString = (char: string[]): string[] => {
+  let map = new Map<string, number>();
+  let ans: string[] = [];
+  for (let i = 0; i < char.length; i++) {
+    map.set(char[i], (map.get(char[i]) || 0) + 1);
+  }
 
-    let map = new Map<string,number>();
-   let ans:string[] = [];
-    for(let i =0; i<char.length; i++){
-        map.set(char[i], (map.get(char[i]) || 0)+1)
-    }
-
-for( const [key, value] of map ){
-
+  for (const [key, value] of map) {
     ans.push(key);
     ans.push(value.toString());
-
-
-}
+  }
   return ans;
+};
 
-}
-
-const input1 = ['a','a','b','b,','c','c','c','d','d','d','d'];
-console.log( "ans1::",compressString(input1))
-export {}
+const input1 = ["a", "a", "b", "b,", "c", "c", "c", "d", "d", "d", "d"];
+console.log("ans1::", compressString(input1));
+export {};
